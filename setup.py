65f202bb84f7b046d0216aa4b13734f79756da05
William Hubbs <w.d.hubbs@gmail.com>
2009-03-17 18:17:01 -0500

    make file/directory layout consistent
    This makes the file/directory layout consistent between this branch ant
    master.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='pybugz',
-    version='0.7.4',
-    py_modules=['bugz'],
-    scripts = ['bugz']
+    name = 'pybugz',
+    version = '0.7.4',
+    author = 'Alastair Tse',
+    author_email = 'alastair@liquidx.net',
+    url = 'http://www.github.com/ColdWind/pybugz',
+    description = 'python interface to bugzilla',
+    packages = ['bugz'],
+    scripts = ['bin/bugz']
 )
