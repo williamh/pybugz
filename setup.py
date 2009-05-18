45255563ae772b306458c66cef078e53798c5984
William Hubbs <w.d.hubbs@gmail.com>
2009-05-17 19:44:03 -0500

    updated setup.py
    Fixed a typo and imported the version from bugz.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+from bugz import __version__
 from distutils.core import setup
 
 setup(
 	name = 'pybugz',
-	version = '0.7.4',
+	version = __version__,
 	description = 'python interface to bugzilla',
 	author = 'Alastair Tse',
 	author_email = 'alastair@liquidx.net',
 	url = 'http://www.liquidx.net/pybuggz',
 	license = "GPL-2",
 	platforms = ['any'],
-	packages = ['bugz']
-	scripts = ['bin/bugz']
+	packages = ['bugz'],
+	scripts = ['bin/bugz'],
 )
