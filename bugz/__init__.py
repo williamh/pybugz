1115baaacd0a913117a72437b8270af49b3670dd
William Hubbs <w.d.hubbs@gmail.com>
2009-07-04 17:56:35 -0500

    released 0.8.0_rc2

--- bugz/__init__.py
+++ bugz/__init__.py
@@ -20,7 +20,7 @@ Classes
 
 """
 
-__version__ = '0.8.0_rc1'
+__version__ = '0.8.0_rc2'
 __author__ = 'Alastair Tse <http://www.liquidx.net/>'
 __contributors__ = ['Santiago M. Mola <cooldwind@gmail.com']
 __revision__ = '$Id: $'
