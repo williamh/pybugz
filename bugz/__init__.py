47ee3be637660e96a5783542db1d0ccbba53791c
William Hubbs <w.d.hubbs@gmail.com>
2011-02-18 18:38:55 -0600

    add myself as a contributor

--- bugz/__init__.py
+++ bugz/__init__.py
@@ -21,7 +21,8 @@ Classes
 
 __version__ = '0.9.0_rc1'
 __author__ = 'Alastair Tse <http://www.liquidx.net/>'
-__contributors__ = ['Santiago M. Mola <cooldwind@gmail.com']
+__contributors__ = ['Santiago M. Mola <cooldwind@gmail.com',
+					'William Hubbs <w.d.hubbs@gmail.com']
 __revision__ = '$Id: $'
 __license__ = """Copyright (c) 2006, Alastair Tse, All rights reserved.
 This following source code is licensed under the GPL v2 License."""
