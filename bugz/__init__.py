fd3773d2137b2c8feba453c0d963bb6c1682ba50
William Hubbs <w.d.hubbs@gmail.com>
2012-04-20 09:55:13 -0500

    move comment describing pybugz to the main script

--- bugz/__init__.py
+++ bugz/__init__.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env python
-
-"""
-Python Bugzilla Interface
-
-Simple command-line interface to bugzilla to allow:
- - searching
- - getting bug info
- - saving attachments
-
-Requirements
-------------
- - Python 2.5 or later
-
-Classes
--------
- - Bugz - Pythonic interface to Bugzilla
- - PrettyBugz - Command line interface to Bugzilla
-
-"""
-
 __version__ = '0.9.3'
 __author__ = 'Alastair Tse <http://www.liquidx.net/>'
 __contributors__ = ['Santiago M. Mola <cooldwind@gmail.com',
