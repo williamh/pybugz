3ee313358b479d019100e94180fbfd0d724c98cf
William Hubbs <w.d.hubbs@gmail.com>
2012-04-25 12:19:43 -0500

    update contributors

--- bugz/__init__.py
+++ bugz/__init__.py
@@ -1,7 +1,8 @@
 __version__ = '0.9.3'
 __author__ = 'Alastair Tse <http://www.liquidx.net/>'
-__contributors__ = ['Santiago M. Mola <cooldwind@gmail.com',
-					'William Hubbs <w.d.hubbs@gmail.com']
+__contributors__ = ['Santiago M. Mola <cooldwind@gmail.com>',
+					'William Hubbs <w.d.hubbs@gmail.com>',
+					'Mike Gilbert <floppym@gentoo.org>']
 __revision__ = '$Id: $'
 __license__ = """Copyright (c) 2006, Alastair Tse, All rights reserved.
 This following source code is licensed under the GPL v2 License."""
