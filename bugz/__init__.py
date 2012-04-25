d0caba2f334fab159543d17a38cb96738bbe6643
William Hubbs <w.d.hubbs@gmail.com>
2012-04-25 13:04:42 -0500

    release pybugz 0.10

--- bugz/__init__.py
+++ bugz/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.9.3'
+__version__ = '0.10'
 __author__ = 'Alastair Tse <http://www.liquidx.net/>'
 __contributors__ = ['Santiago M. Mola <cooldwind@gmail.com>',
 					'William Hubbs <w.d.hubbs@gmail.com>',
