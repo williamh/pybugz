a983b9e8667829bc2a07c022e3cfe7fc5e0d7e4c
Pavel Raiskup <pavel@raiskup.cz>
2013-01-14 20:53:12 +0100

    log: added interface for logging
    * bugz/log.py: New file.

---
+++ bugz/log.py
@@ -0,0 +1,45 @@
+# in this file should be unique way to logger feature
+
+LogSettins = {
+	'W' : {
+		'symb' : '!',
+		'word' : 'Warn',
+	},
+	'E' : {
+		'symb' : '#',
+		'word' : 'Error',
+	},
+	'D' : {
+		'symb' : '~',
+		'word' : 'Dbg',
+	},
+	'!' : {
+		'symb' : '!',
+		'word' : 'UNKNWN',
+	},
+}
+
+def formatOut(msg, id='!'):
+	lines = str(msg).split('\n')
+	start = True
+	symb=LogSettins[id]['symb']
+	word=LogSettins[id]['word'] + ":"
+
+	for line in lines:
+		print ' ' + symb + ' ' + line
+
+def log_error(string):
+	formatOut(string, 'E')
+	return
+
+def log_warn(string):
+	formatOut(string, 'W')
+	return
+
+def log_info(string):
+	formatOut(string, 'I')
+	return
+
+def log_debug(string):
+	formatOut(string, 'D')
+	return
