128ac8d724d7994477702a4b7455e207bf6b4d37
William Hubbs <w.d.hubbs@gmail.com>
2014-09-01 17:31:29 -0500

    Setup: install man page relative to sys.prefix

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ import os
 from distutils.core import setup
 from bugz import __version__
 
-# when using virtualenv; trust that man will find the nearby directory
-if 'VIRTUAL_ENV' in os.environ:
-	man_pages = 'man/man1'
-else:
-	man_pages = '/usr/share/man/man1'
-
 setup(
 	name = 'pybugz',
 	version = __version__,
@@ -20,6 +14,6 @@ setup(
 	packages = ['bugz'],
 	scripts = ['bin/bugz'],
 	data_files = [
-		(man_pages, ['man/bugz.1']),
+		('share/man/man1', ['man/bugz.1']),
 	],
 )
