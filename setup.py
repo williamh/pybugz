98a9ce28c7702dbbcb636dbcbfcd09cca2e607d9
William Hubbs <w.d.hubbs@gmail.com>
2014-10-01 12:42:45 -0500

    style fixes in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 import glob
-import os
 from distutils.core import setup
 from bugz import __version__
 
 setup(
-	name = 'pybugz',
-	version = __version__,
-	description = 'python interface to bugzilla',
-	author = 'Alastair Tse',
-	author_email = 'alastair@liquidx.net',
-	url = 'http://www.liquidx.net/pybugz',
-	license = "GPL-2",
-	platforms = ['any'],
-	packages = ['bugz'],
-	scripts = ['bin/bugz'],
-	data_files = [
+	name='pybugz',
+	version=__version__,
+	description='python interface to bugzilla',
+	author='Alastair Tse',
+	author_email='alastair@liquidx.net',
+	url='http://www.liquidx.net/pybugz',
+	license="GPL-2",
+	platforms=['any'],
+	packages=['bugz'],
+	scripts=['bin/bugz'],
+	data_files=[
 		('share/man/man1', ['man/bugz.1']),
 		('share/man/man5', ['man/pybugz.d.5']),
 		('share/pybugz.d', glob.glob('pybugz.d/*.conf')),
