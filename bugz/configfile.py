de8881539e7bf317f7dc4771f356bc326af90abb
William Hubbs <w.d.hubbs@gmail.com>
2015-01-02 11:02:42 -0600

    Refactor configuration file processing
    Move all configuration file processing into the configfile module.

--- bugz/configfile.py
+++ bugz/configfile.py
@@ -6,9 +6,18 @@ import sys
 from bugz.log import log_error
 
 
-def read_config(parser, ConfigFiles):
+def load_config(UserConfig=None):
+	parser = configparser.ConfigParser(default_section='default')
+	DefaultConfigs = sorted(glob.glob(sys.prefix + '/share/pybugz.d/*.conf'))
+	SystemConfigs = sorted(glob.glob('/etc/pybugz.d/*.conf'))
+	if UserConfig is not None:
+		UserConfig = os.path.expanduser(UserConfig)
+	else:
+		UserConfig = os.path.expanduser('~/.bugzrc')
+
 	try:
-		parser.read(ConfigFiles)
+		parser.read(DefaultConfigs + SystemConfigs + [UserConfig])
+
 	except configparser.DuplicateOptionError as e:
 		log_error(e)
 		sys.exit(1)
@@ -22,28 +31,20 @@ def read_config(parser, ConfigFiles):
 		log_error(e)
 		sys.exit(1)
 
-
-def get_config(parser, UserConfig=None):
-	DefaultConfigs = sorted(glob.glob(sys.prefix + '/share/pybugz.d/*.conf'))
-	SystemConfigs = sorted(glob.glob('/etc/pybugz.d/*.conf'))
-	if UserConfig is not None:
-		UserConfig = os.path.expanduser(UserConfig)
-	else:
-		UserConfig = os.path.expanduser('~/.bugzrc')
-	read_config(parser, DefaultConfigs)
-	read_config(parser, SystemConfigs)
-	read_config(parser, UserConfig)
+	return parser
 
 
 def get_config_option(get, section, option):
 	try:
 		value = get(section, option)
-		if value == '':
-			log_error('{0} is not set'.format(option))
-			sys.exit(1)
 
-		return value
 	except ValueError as e:
 		log_error('{0} is not in the right format: {1}'.format(option,
 			str(e)))
 		sys.exit(1)
+
+	if value == '':
+		log_error('{0} is not set'.format(option))
+		sys.exit(1)
+
+	return value
