4dec396d43061c1def9aa19601c89c8aa00742b3
William Hubbs <w.d.hubbs@gmail.com>
2016-07-09 14:46:02 -0500

    setup.py: add documentation and examples

--- setup.py
+++ setup.py
@@ -1,23 +1,86 @@
-import glob
-from setuptools import setup
-
+from setuptools import setup,find_packages
+from glob import glob
 from bugz.definitions import __version__
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pybugz',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
     version=__version__,
+
     description='python interface to bugzilla',
-    long_description='python interface to bugzilla',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='http://github.com/williamh/pybugz',
+
+    # Author details
     author='Alastair Tse',
     author_email='alastair@liquidx.net',
+
+    # Choose your license
     license="GPL-2",
-    url='http://www.liquidx.net/pybugz',
-    packages=['bugz'],
-    platforms=['any'],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    # py_modules=["my_module"],
+
+    # List run-time dependencies here. These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    # install_requires=['sample_package'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
     data_files=[
         ('share/man/man1', ['man/bugz.1']),
         ('share/man/man5', ['man/pybugz.d.5']),
-        ('share/pybugz.d', glob.glob('pybugz.d/*.conf')),
+        ('share/pybugz.d', glob('pybugz.d/*.conf')),
     ],
-    entry_points={'console_scripts': ['bugz=bugz.cli:main']},
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'bugz=bugz.cli:main'
+        ]
+    },
 )
